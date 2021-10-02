6feb2c7cc22ea24c871a9bc20076df9a38802cea
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-10-02 07:06:57 -0400

    initial commit

---
+++ src/data/schedule/scheduleData21.js
@@ -0,0 +1,30 @@
+const schedule = {
+  "Week 1": {
+    210101: {
+      homeTeam: "Atlanta Falcons",
+      awayTeam: "Philadelphia Eagles",
+      gameDate: "Sun Sep 13 2021",
+      gameTime: "13:00:00",
+      homeTeamScore: 6,
+      awayTeamScore: 32,
+    },
+    210102: {
+      homeTeam: "Buffalo Bills",
+      awayTeam: "Pittsburgh Steelers",
+      gameDate: "Sun Sep 13 2021",
+      gameTime: "13:00:00",
+      homeTeamScore: 16,
+      awayTeamScore: 23,
+    },
+    210103: {
+      homeTeam: "Cincinnati Bengals",
+      awayTeam: "Minnesota Vikings",
+      gameDate: "Sun Sep 13 2021",
+      gameTime: "13:00:00",
+      homeTeamScore: 27,
+      awayTeamScore: 24,
+    },
+  },
+};
+
+export { schedule };
