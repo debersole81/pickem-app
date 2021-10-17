03c6231bf2f11fccce05baa9e34fe4248e5d9889
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-10-17 15:07:41 -0400

    build form template

--- src/data/components/FormFields.js
+++ src/data/components/FormFields.js
@@ -1,14 +1,27 @@
 import { schedule21 } from "../schedule/scheduleData21.js";
 
-// const initialFormState = {
-
-// }
-
 const FormFields = () => {
   console.log(schedule21);
 
-
-  return null;
+  return (
+    <form>
+      <label htmlFor="week">Week:</label>
+      <input type="text" id="week" name="week"></input>
+      <br />
+      <label htmlFor="gameDate">Date:</label>
+      <input type="date" id="gameDate" name="gameDate"></input>
+      <br />
+      <label htmlFor="gameTime">Time:</label>
+      <input type="time" id="gameTime" name="gameTime"></input>
+      <br />
+      <label htmlFor="homeTeam">Home Team:</label>
+      <input type="text" id="homeTeam" name="homeTeam"></input>
+      <br />
+      <label htmlFor="awayTeam">Away Team:</label>
+      <input type="text" id="awayTeam" name="awayTeam"></input>
+      <br />
+    </form>
+  );
 };
 
 export default FormFields;
