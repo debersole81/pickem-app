3697e0b2f02c2dfb0243e65c05819088c323866c
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-10-17 14:33:50 -0400

    update schedule21 import

---
+++ src/data/components/FormFields.js
@@ -0,0 +1,14 @@
+import { schedule21 } from "../schedule/scheduleData21.js";
+
+// const initialFormState = {
+
+// }
+
+const FormFields = () => {
+  console.log(schedule21);
+
+
+  return null;
+};
+
+export default FormFields;
