5e66e8a27eb4a42573c2388feadf125186dbb455
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-10-17 15:43:45 -0400

    build initial form state object and change handler

--- src/data/components/FormFields.js
+++ src/data/components/FormFields.js
@@ -1,26 +1,73 @@
 import { schedule21 } from "../schedule/scheduleData21.js";
+import { useState } from "react";
+
+const initialFormState = {
+  week: 0,
+  gameDate: "",
+  gameTime: "",
+  homeTeam: "",
+  awayTeam: "",
+};
 
 const FormFields = () => {
+  const [formState, setFormState] = useState(initialFormState);
+
+  /**Form change handler */
+  const onChange = (e) => {
+    setFormState(() => ({ ...formState, [e.target.name]: e.target.value }));
+  };
+
+  console.log(formState);
   console.log(schedule21);
 
   return (
     <form>
       <label htmlFor="week">Week:</label>
-      <input type="text" id="week" name="week"></input>
+      <input
+        type="number"
+        id="week"
+        name="week"
+        value={formState.week}
+        onChange={onChange}
+      ></input>
       <br />
       <label htmlFor="gameDate">Date:</label>
-      <input type="date" id="gameDate" name="gameDate"></input>
+      <input
+        type="date"
+        id="gameDate"
+        name="gameDate"
+        value={formState.gameDate}
+        onChange={onChange}
+      ></input>
       <br />
       <label htmlFor="gameTime">Time:</label>
-      <input type="time" id="gameTime" name="gameTime"></input>
+      <input
+        type="time"
+        id="gameTime"
+        name="gameTime"
+        value={formState.gameTime}
+        onChange={onChange}
+      ></input>
       <br />
       <label htmlFor="homeTeam">Home Team:</label>
-      <input type="text" id="homeTeam" name="homeTeam"></input>
+      <input
+        type="text"
+        id="homeTeam"
+        name="homeTeam"
+        value={formState.homeTeam}
+        onChange={onChange}
+      ></input>
       <br />
       <label htmlFor="awayTeam">Away Team:</label>
-      <input type="text" id="awayTeam" name="awayTeam"></input>
+      <input
+        type="text"
+        id="awayTeam"
+        name="awayTeam"
+        value={formState.awayTeam}
+        onChange={onChange}
+      ></input>
       <br />
-      <input type="submit" id="submit"></input>
+      <input type="submit" id="submit" value="Submit"></input>
     </form>
   );
 };
