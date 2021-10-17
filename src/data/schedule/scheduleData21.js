c2ddc60b0c8f94a764a372021f3f05e3e1e2d56a
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-10-17 14:32:33 -0400

    import object constructors and fix object build

--- src/data/schedule/scheduleData21.js
+++ src/data/schedule/scheduleData21.js
@@ -1,30 +1,14 @@
-const schedule = {
-  "Week 1": {
-    210101: {
-      homeTeam: "Atlanta Falcons",
-      awayTeam: "Philadelphia Eagles",
-      gameDate: "Sun Sep 13 2021",
-      gameTime: "13:00:00",
-      homeTeamScore: 6,
-      awayTeamScore: 32,
-    },
-    210102: {
-      homeTeam: "Buffalo Bills",
-      awayTeam: "Pittsburgh Steelers",
-      gameDate: "Sun Sep 13 2021",
-      gameTime: "13:00:00",
-      homeTeamScore: 16,
-      awayTeamScore: 23,
-    },
-    210103: {
-      homeTeam: "Cincinnati Bengals",
-      awayTeam: "Minnesota Vikings",
-      gameDate: "Sun Sep 13 2021",
-      gameTime: "13:00:00",
-      homeTeamScore: 27,
-      awayTeamScore: 24,
-    },
-  },
-};
+import { Week, Game } from "../constructors/scheduleConstructor";
 
-export { schedule };
+const week = new Week();
+const game = new Game();
+
+const schedule21 = [
+  week[
+    {
+      game: game,
+    }
+  ],
+];
+
+export { schedule21 };
